aaac4ef09287f424cb22d9ecacdc322cbeebac9b
Daniel Way <projects@danieldway.com>
2020-01-28 11:45:03 -0600

    feat: asdf

--- src/test-service.ts
+++ src/test-service.ts
@@ -1,9 +1,9 @@
 export class TestService {
   public test () {
-    return 'Hello world!!'
+    return 'Hello world 2!'
   }
 
   public test2 () {
-    return 'Hello world 2!';
+    return 'Hello test 2';
   }
 }
